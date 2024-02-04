193a521a5b4a1b24b76b4dc6b38b46c8db29b185
alessandro ferreira <argferreira1@gmail.com>
2024-02-04 17:42:06 -0300

    Feature: Testes de Integracao

---
+++ tests/integration/authRoutes.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import app from '../app'; //ToDo
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('Testes das rotas de autenticação', () => {
+  it('deve autenticar com credenciais válidas e retornar um token JWT', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({
+        login: process.env.AUTH_USER,
+        senha: process.env.AUTH_PASSWORD
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    // Verificações adicionais podem ser feitas aqui
+  });
+
+  it('não deve autenticar com credenciais inválidas', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({
+        login: 'usuarioInvalido',
+        senha: 'senhaInvalida'
+      });
+
+    expect(response.status).toBe(401);
+  });
+});
---
+++ tests/integration/cardRoutes.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import app from '../app'; //ToDo
+
+describe('Testes das rotas de card', () => {
+  it('deve criar um novo card', async () => {
+    const cardData = { titulo: 'Novo Card', conteudo: 'Conteúdo do card', lista: 'Lista 1' };
+    const response = await request(app)
+      .post('/cards')
+      .send(cardData);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('deve listar todos os cards', async () => {
+    const response = await request(app)
+      .get('/cards');
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
+
+});
