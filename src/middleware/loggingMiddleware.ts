e49a157a1188ad0807032265e64964dd8aebec1a
alessandro ferreira <argferreira1@gmail.com>
2024-02-04 19:25:24 -0300

    Feature: Criacao dos middlewares

---
+++ src/middleware/loggingMiddleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const loggingMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+    const { method, path } = req;
+    const date = new Date().toISOString();
+
+    console.log(`${date} - ${method} ${path}`);
+
+    next();
+};
