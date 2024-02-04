9ea76a0fece1ea8bd5ef047e540705aaaac7dfa9
alessandro ferreira <argferreira1@gmail.com>
2024-02-04 15:31:33 -0300

    Feature: Testes unitados para os controllers

---
+++ tests/unit/controllers/AuthController.test.ts
@@ -0,0 +1,42 @@
+import { AuthController } from '../controllers/AuthController'; //ToDo
+import { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('Testes do AuthController', () => {
+  let authController: AuthController;
+  let requisicaoMock: Partial<Request>;
+  let respostaMock: Partial<Response>;
+  let resposta: any;
+
+  beforeEach(() => {
+    authController = new AuthController();
+    requisicaoMock = {};
+    respostaMock = {
+      json: jest.fn().mockImplementation((resultado) => {
+        resposta = resultado;
+        return respostaMock as Response;
+      }),
+      status: jest.fn().mockReturnThis(),
+    };
+  });
+
+  it('deve retornar um token JWT quando as credenciais são válidas', async () => {
+    requisicaoMock.body = { login: process.env.AUTH_USER, senha: process.env.AUTH_PASSWORD };
+
+    await authController.login(requisicaoMock as Request, respostaMock as Response);
+
+    expect(respostaMock.status).toHaveBeenCalledWith(200);
+    expect(resposta).toHaveProperty('token');
+  });
+
+  it('deve retornar um status de erro quando as credenciais são inválidas', async () => {
+    requisicaoMock.body = { login: 'usuarioInvalido', senha: 'senhaInvalida' };
+
+    await authController.login(requisicaoMock as Request, respostaMock as Response);
+
+    expect(respostaMock.status).toHaveBeenCalledWith(401);
+  });
+
+});
---
+++ tests/unit/controllers/CardController.test.ts
@@ -0,0 +1,60 @@
+import { CardController } from '../controllers/CardController'; //ToDo
+import { Request, Response } from 'express';
+
+describe('Testes do CardController', () => {
+  let cardController: CardController;
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let response: any;
+
+  beforeEach(() => {
+    cardController = new CardController();
+    mockRequest = {};
+    mockResponse = {
+      json: jest.fn().mockImplementation((result) => {
+        response = result;
+        return mockResponse as Response;
+      }),
+      status: jest.fn().mockReturnThis(),
+    };
+  });
+
+  it('deve criar um card e retornar o card com ID', async () => {
+    mockRequest.body = { titulo: 'Novo Card', conteudo: 'Conteúdo do card', lista: 'Lista 1' };
+
+    await cardController.createCard(mockRequest as Request, mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(201);
+    expect(response).toHaveProperty('id');
+    expect(response.titulo).toEqual('Novo Card');
+  });
+
+  it('deve listar todos os cards', async () => {
+    await cardController.getAllCards(mockRequest as Request, mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(Array.isArray(response)).toBeTruthy();
+  });
+
+  it('deve atualizar um card existente', async () => {
+    const cardId = 1; // Exemplo de ID de card
+    mockRequest.params = { id: cardId.toString() };
+    mockRequest.body = { titulo: 'Card Atualizado', conteudo: 'Conteúdo atualizado', lista: 'Lista 2' };
+
+    await cardController.updateCard(mockRequest as Request, mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(response.id).toEqual(cardId);
+    expect(response.titulo).toEqual('Card Atualizado');
+  });
+
+  it('deve deletar um card', async () => {
+    const cardId = 1; // Exemplo de ID de card
+    mockRequest.params = { id: cardId.toString() };
+
+    await cardController.deleteCard(mockRequest as Request, mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+  });
+
+});
