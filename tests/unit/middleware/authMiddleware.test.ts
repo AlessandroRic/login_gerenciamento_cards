32775056d66ccacde759a0543530e234b451b172
alessandro ferreira <argferreira1@gmail.com>
2024-02-04 15:44:35 -0300

    Feature: Testes unitarios para os middlewares

---
+++ tests/unit/middleware/authMiddleware.test.ts
@@ -0,0 +1,60 @@
+import { Request, Response, NextFunction } from 'express';
+import { authMiddleware } from '../middleware/authMiddleware'; //ToDo
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('Testes do authMiddleware', () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+  });
+
+  it('deve chamar next() quando as credenciais são válidas', () => {
+    const validToken = jwt.sign({ user: process.env.AUTH_USER }, process.env.JWT_SECRET as string);
+    mockRequest = {
+      headers: {
+        authorization: `Bearer ${validToken}`
+      }
+    };
+
+    authMiddleware(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(nextFunction).toHaveBeenCalled();
+  });
+
+  it('deve retornar status 401 para um token JWT inválido', () => {
+    mockRequest = {
+      headers: {
+        authorization: 'Bearer tokenInvalido'
+      }
+    };
+
+    authMiddleware(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+
+    expect(mockResponse.json).toHaveBeenCalledWith({ mensagem: 'Token inválido ou não fornecido' });
+  });
+
+  it('deve retornar status 401 se não houver token', () => {
+    mockRequest = {
+      headers: {}
+    };
+
+    authMiddleware(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+
+    expect(mockResponse.json).toHaveBeenCalledWith({ mensagem: 'Token inválido ou não fornecido' });
+  });
+
+});
---
+++ tests/unit/middleware/loggingMiddleware.test.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { loggingMiddleware } from '../middleware/loggingMiddleware'; //ToDo
+
+describe('Testes do loggingMiddleware', () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+  const originalConsoleLog = console.log;
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {};
+    console.log = jest.fn();
+  });
+
+  afterEach(() => {
+    console.log = originalConsoleLog;
+  });
+
+  it('deve registrar informações no console para requisições de alteração', () => {
+    mockRequest.method = 'PUT';
+    mockRequest.url = '/cards/1';
+    mockRequest.body = { titulo: 'Card Atualizado', conteudo: 'Conteúdo atualizado', lista: 'Lista 2' };
+
+    loggingMiddleware(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining('Card 1 - Card Atualizado - Alterar'));
+    expect(nextFunction).toHaveBeenCalled();
+  });
+
+  it('deve registrar informações no console para requisições de remoção', () => {
+    mockRequest.method = 'DELETE';
+    mockRequest.url = '/cards/1';
+
+    loggingMiddleware(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining('Card 1 - Removido'));
+    expect(nextFunction).toHaveBeenCalled();
+  });
+
+});
