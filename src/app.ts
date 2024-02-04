b29d98dbfc351dec5a881110ac4c8782ec2c5355
alessandro ferreira <argferreira1@gmail.com>
2024-02-04 17:53:34 -0300

    Feature: Base para o app e servidor

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+
+// Importando as rotas
+import authRoutes from './routes/authRoutes';
+import cardRoutes from './routes/cardRoutes';
+
+// Importando os middlewares
+import authMiddleware from './middleware/authMiddleware';
+import loggingMiddleware from './middleware/loggingMiddleware';
+
+const app: Express = express();
+
+// Configuração do bodyParser para receber requisições JSON
+app.use(bodyParser.json());
+
+// Configurando as rotas
+app.use('/login', authRoutes);
+app.use('/cards', authMiddleware, loggingMiddleware, cardRoutes);
+
+// Configuração para lidar com rotas não encontradas
+app.use((req, res) => {
+    res.status(404).send('Página não encontrada');
+});
+
+export default app;
